36cd77c8b89cca5859c6869fe4cb5351806b3d10
PoliceAmunice <sundov97@gmail.com>
2022-05-13 09:44:23 +1000

    bug-fix

--- compare-versions.js
+++ compare-versions.js
@@ -6,16 +6,19 @@ export function compareVersions(v1, v2) {
 }
 
 function compare(arr1, arr2) {
-   const el1 = Number.parseInt(arr1.shift());
-   const el2 = Number.parseInt(arr2.shift());
+   const el1 = +arr1.shift();
+   const el2 = +arr2.shift();
 
    if (el1 < el2) {
       return -1;
    } else if (el1 > el2) {
       return 1;
-   } else if (arr1.length !== 0) {
-      return compare(arr1, arr2);
+   } else if (!!arr1[0] === !!arr2[0]) {
+      return arr1[0] ? compare(arr1, arr2) : 0;
    } else {
-      return 0;
+      return !arr1[0]
+         ? compare(['0'], arr2)
+         : compare(arr1, ['0'])
+      ;
    }
 }
