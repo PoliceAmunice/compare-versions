a087a9b4b60f7a60ffd13de561200a42bab8729a
sundov_ep <sundov_ep@farpost.com>
2022-05-13 14:54:14 +1000

    added cycle comparison

--- compare-versions.js
+++ compare-versions.js
@@ -1,4 +1,4 @@
-export function compareVersions(v1, v2) {
+export function compareVersionsRecursive(v1, v2) {
    const arr1 = v1.split('.');
    const arr2 = v2.split('.');
 
@@ -17,5 +17,25 @@ function compare(arr1, arr2) {
       return 0;
    } else {
       return compare(arr1, arr2);
+   }
+}
+
+export function compareVersionsCycle(v1, v2) {
+   const arr1 = v1.split('.');
+   const arr2 = v2.split('.');
+
+   let el1 = 0, el2 = 0;
+
+   while (true) {
+      el1 = +arr1.shift() || 0;
+      el2 = +arr2.shift() || 0;
+
+      if (el1 < el2) {
+         return -1;
+      } else if (el1 > el2) {
+         return 1;
+      } else if (!arr1[0] && !arr2[0]) {
+         return 0;
+      }
    }
 }
